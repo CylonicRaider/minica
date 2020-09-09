8503f65272c73087b201fc4ef171df9e126378e6
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-09-09 22:01:43 +0200

    Commence OpenSSL command-line driver

--- minica.py
+++ minica.py
@@ -5,6 +5,22 @@
 Simple local X.509 certificate management.
 """
 
+import subprocess
+
+OPENSSL_PATH = '/usr/bin/openssl'
+
+class OpenSSLDriver:
+    def __init__(self):
+        self.openssl_path = OPENSSL_PATH
+
+    def _run_openssl(self, args, input):
+        proc = subprocess.Popen((self.openssl_path,) + tuple(args),
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        stdin, stdout = proc.communicate(input)
+        status = proc.wait()
+        return {'status': status, 'stdin': stdin, 'stdout': stdout}
+
 def main():
     raise NotImplementedError
 
